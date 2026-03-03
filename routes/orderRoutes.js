867329f33891269de65544e3bba0064278f52ff8
hassan-jo <hassanmoahmed09@gmail.com>
2026-03-03 06:53:28 -0800

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,14 +6,73 @@ const orderController = require("../controllers/orderController");
 const jwtValidate = require("../middlewares/jwtValidate");
 const adminValidate = require("../middlewares/adminValidate");
 
-// Checkout User
+/**
+ * @swagger
+ * tags:
+ *   name: Orders
+ *   description: Order management APIs
+ */
+
+/**
+ * @swagger
+ * /orders/checkout:
+ *   post:
+ *     summary: Checkout user cart
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       201:
+ *         description: Order created successfully
+ */
 router.post("/checkout", jwtValidate, orderController.checkout);
 
-// Admin Controls
+/**
+ * @swagger
+ * /orders:
+ *   get:
+ *     summary: Get all orders (Admin only)
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of orders
+ */
 router.get("/", jwtValidate, adminValidate, orderController.getAllOrders);
 
+/**
+ * @swagger
+ * /orders/my:
+ *   get:
+ *     summary: Get user orders
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User orders list
+ */
 router.get("/my", jwtValidate, orderController.getMyOrders);
 
+/**
+ * @swagger
+ * /orders/{id}:
+ *   put:
+ *     summary: Update order (Admin only)
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Order updated
+ */
 router.put(
   "/:id",
   jwtValidate,
@@ -21,6 +80,18 @@ router.put(
   orderController.updateOrder
 );
 
+/**
+ * @swagger
+ * /orders/{id}:
+ *   delete:
+ *     summary: Delete order (Admin only)
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Order deleted
+ */
 router.delete(
   "/:id",
   jwtValidate,
